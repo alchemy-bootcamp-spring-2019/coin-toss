7e1abbf7d861213e8059cd019c8dc0b035d4d58b
willemlarsen@gmail.com <willem@alchemycodelab.com>
2019-04-06 13:42:21 -0700

    flipCoinAndReveal image

--- src/index.js
+++ src/index.js
@@ -9,10 +9,24 @@ const lossCount = document.getElementById('loss-count');
 let wins = 0;
 let losses = 0;
 
+function flipCoinAndReveal() {
+    const random = Math.random();
+    const tossedSide = getHeadsOrTails(random);
+    let imageSource = '';
+    if(tossedSide === 'heads') {
+        imageSource = 'assets/heads.jpg';
+    }
+    else {
+        imageSource = 'assets/tails.jpg';
+    }
+    coinImage.src = imageSource;
+}
+
+
 pickForm.addEventListener('submit', function(event) {
     event.preventDefault();
 
     const userChoice = pickForm.elements.side.value;
 
-
+    flipCoinAndReveal();
 });
